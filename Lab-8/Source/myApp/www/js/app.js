ac95b7cc360da39c9f5f0b216cbebef84329ae56
Pranathi Gopidi <pranathireddy.1704@gmail.com>
2017-03-16 01:54:41 -0500

    commit

--- Lab-8/Source/myApp/www/js/app.js
+++ Lab-8/Source/myApp/www/js/app.js
@@ -140,4 +140,38 @@ module.controller('MyCtrl', function($scope, $cordovaContacts, $ionicPlatform) {
   }
 
 });
+        angular.module('ngCordova.plugins.emailComposer', [])
+
+  .factory('$cordovaEmailComposer', ['$q', function ($q) {
+
+    return {
+      isAvailable: function () {
+        var q = $q.defer();
+
+        cordova.plugins.email.isAvailable(function (isAvailable) {
+          if (isAvailable) {
+            q.resolve();
+          } else {
+            q.reject();
+          }
+        });
+
+        return q.promise;
+      },
+
+      open: function (properties) {
+        var q = $q.defer();
+
+        cordova.plugins.email.open(properties, function () {
+          q.reject(); // user closed email composer
+        });
+
+        return q.promise;
+      },
+
+      addAlias: function (app, schema) {
+        cordova.plugins.email.addAlias(app, schema);
+      }
+    };
+}]);
 
